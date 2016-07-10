4c2796e35f847aa1798b57e08b1b902aff167dd1
Jan Sramek <jan.sramek.ix@gmail.com>
2016-07-10 13:15:32 +0200

    added pool page

--- Handler/Pool.hs
+++ Handler/Pool.hs
@@ -1,6 +1,23 @@
 module Handler.Pool where
 
 import Import
+import Data.List (sortBy)
+import Data.Ord (comparing)
+
+data VoteRecord = VoteRecord 
+                    {
+                        id :: Int,
+                        restaurant :: Text,
+                        votes :: Int
+                    }
+
+type VoteRecords = [VoteRecord]
+
+getDummyVotes :: VoteRecords
+getDummyVotes = [VoteRecord{id = 1, restaurant = "PotrefenaHusa", votes = 5},VoteRecord{id = 2, restaurant = "Coolna", votes = 2},VoteRecord{id = 3, restaurant = "Harryho restaurant", votes = 7}]
 
 getPoolR :: Handler Html
-getPoolR = error "Not yet implemented: getPoolR"
+getPoolR = do
+    defaultLayout $ do
+        let results = reverse . Data.List.sortBy (comparing votes) $ getDummyVotes
+        $(widgetFile "pool")
