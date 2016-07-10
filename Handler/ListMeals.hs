7e7cc028ee8e1c99b6a5fb77dfb8b0447cc3fcc0
Jan Sramek <jan.sramek.ix@gmail.com>
2016-07-11 01:02:24 +0200

    IP logging

--- Handler/ListMeals.hs
+++ Handler/ListMeals.hs
@@ -3,8 +3,22 @@ module Handler.ListMeals where
 import Import
 import Handler.Cache
 
+getIP = do
+    ip1 <- lookupHeader "X-Real-IP"
+    ip2 <- lookupHeader "X-Forwarded-For" 
+    case ip1 of 
+                      Just ip -> return ip
+                      Nothing -> case ip2 of
+                                      Just ip -> return ip
+                                      Nothing -> error "Cannot get client IP"
+
+
+
 getListMealsR :: Handler Html
 getListMealsR = do
     defaultLayout $ do
         let menuList = getData 
+        clientIp <- getIP
+        liftIO $ print ("Client IP: " ++ clientIp)
         $(widgetFile "list")
+
