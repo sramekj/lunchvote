3722ccc0f8794f0bf6041cd9a211809f1a07e3f1
Jan Sramek <jan.sramek.ix@gmail.com>
2016-07-10 15:08:29 +0200

    redone pool logic

--- Handler/Pool.hs
+++ Handler/Pool.hs
@@ -18,15 +18,20 @@ type VoteRecords = Map Int Int
 getDummyVotes :: VoteRecords
 getDummyVotes = M.fromList [(0,5), (1,2), (2,7)]
 
+getRestaurantName :: Int -> [(Int,Text)] -> Text
+getRestaurantName index restaurantList = let restaurantItem = L.find (\k -> fst k == index) restaurantList
+                                         in case restaurantItem of
+                                                Just (_, x) -> x
+                                                otherwise -> error "Invalid key"
+
 getVoteData :: VoteRecords -> MenuList -> [VoteResult]
-getVoteData recordMap menuList = 
-                        [VoteResult{name="hovno", votes=5}]
-    
---reverse . L.sortBy (comparing snd) 
-            
+getVoteData recordMap menuList = let restaurants = fmap (\k -> (Handler.ListMeals.id k, restaurant k)) menuList
+                                     sortedVotes = M.toDescList recordMap
+                                 in fmap (\k -> VoteResult{name = getRestaurantName (fst k) restaurants, votes = snd k}) sortedVotes
+                                 
 
 getPoolR :: Handler Html
 getPoolR = do
     defaultLayout $ do
-        let results = getVoteData getDummyVotes ([]::MenuList)
+        let results = getVoteData getDummyVotes getDummyData
         $(widgetFile "pool")
