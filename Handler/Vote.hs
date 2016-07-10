02a3582194a7d7179d18621c57cd78f612ed272f
Jan Sramek <jan.sramek.ix@gmail.com>
2016-07-10 23:24:56 +0200

    fixed js

--- Handler/Vote.hs
+++ Handler/Vote.hs
@@ -2,8 +2,9 @@ module Handler.Vote where
 
 import Import
 import Handler.Cache
+import qualified Network.HTTP.Types as H
 
 postVoteR :: Int -> Handler Html
 postVoteR restaurantId = do
+    runDB $ updateWhere [PoolRestaurantId ==. restaurantId] [PoolVotes +=. 1]
     defaultLayout [whamlet||]
-
