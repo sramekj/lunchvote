7c48bddf8145c013ed9d44c5336dcc051b7ef3ea
Jan Sramek <jan.sramek.ix@gmail.com>
2016-07-16 16:20:38 +0200

    simple restyle

--- Handler/ListMeals.hs
+++ Handler/ListMeals.hs
@@ -8,26 +8,6 @@ getListMealsR :: Handler Html
 getListMealsR = do
     menuList <- lift $ getData 
     canVote <- validateVoterIp
---  if canVote then insertVoterIp
---             else $(logInfo) "User cannot vote"
-
-    
-
-
-  --   case lift $ canVote of True -> return ()
-  --                          False -> return ()
-
---      let keke = getData'
---      ids <- liftIO getRestaurantIds
-----      print ids
---        print <$> ids
-
---        json <- getJSON resKey
---        liftIO $ print json
-
-        
---     clientIp <- getIP
---     liftIO $ print ("Client IP: " ++ clientIp)
     defaultLayout $ do
         $(widgetFile "list")
 
