a2aa6cca65c1da6f1b52b4cf93bcb2076b7b89a0
Jan Sramek <jan.sramek.ix@gmail.com>
2016-07-16 17:49:45 +0200

    purge check

--- Handler/Purge.hs
+++ Handler/Purge.hs
@@ -1,13 +1,24 @@
 module Handler.Purge where
 
 import Import
+import Handler.Common
 
 purge = runDB $ do
     deleteWhere ([] :: [Filter Pool])
     deleteWhere ([] :: [Filter Votes])
  
+compareIp myIp = do
+    ip <- getIP
+    return $ ip == myIp
+
 getPurgeR :: Handler Html
 getPurgeR = do
-    purge
-    defaultLayout $ do
-        [whamlet|<h1>DB purged!|]
+    approval <- compareIp "127.0.0.1"
+    case approval of 
+            True -> do
+                purge
+                defaultLayout $ do
+                    [whamlet|<div .jumbotron .text-center><h1>DB purged!|]
+            False -> do
+                defaultLayout $ do
+                    [whamlet|<div .jumbotron .text-center><h1>Nice try!|]
